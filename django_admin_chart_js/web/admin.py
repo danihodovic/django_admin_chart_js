d8fc3a68ad68f3bea139b0a4c888f006ef97c286
Dani Hodovic <dani.hodovic@gmail.com>
2019-08-09 16:19:51 +0200

    Dynamic loading of chart data

--- django_admin_chart_js/web/admin.py
+++ django_admin_chart_js/web/admin.py
@@ -4,6 +4,8 @@ from django.contrib import admin
 from django.core.serializers.json import DjangoJSONEncoder
 from django.db.models import Count
 from django.db.models.functions import TruncDay
+from django.http import JsonResponse
+from django.urls import path
 
 from .models import EmailSubscriber
 
@@ -13,14 +15,32 @@ class EmailSubscriberAdmin(admin.ModelAdmin):
     list_display = ("id", "email", "created_at")
     ordering = ("-created_at",)
 
+    # Inject chart data on page load in the ChangeList view
     def changelist_view(self, request, extra_context=None):
-        chart_data = (
+        chart_data = self.chart_data()
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+        return super().changelist_view(request, extra_context=extra_context)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        extra_urls = [
+            path("chart_data/", self.admin_site.admin_view(self.chart_data_endpoint))
+        ]
+        # NOTE! Our custom urls have to go before the default urls, because they
+        # default ones match anything.
+        return extra_urls + urls
+
+    # JSON endpoint for generating chart data that is used for dynamic loading
+    # via JS.
+    def chart_data_endpoint(self, request):
+        chart_data = self.chart_data()
+        return JsonResponse(list(chart_data), safe=False)
+
+    def chart_data(self):
+        return (
             EmailSubscriber.objects.annotate(date=TruncDay("created_at"))
             .values("date")
             .annotate(y=Count("id"))
             .order_by("-date")
         )
-
-        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
-        extra_context = extra_context or {"chart_data": as_json}
-        return super().changelist_view(request, extra_context=extra_context)
